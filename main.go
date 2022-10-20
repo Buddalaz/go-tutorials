05c05a80afecd889f3c33c64f2a49b782cd10fbc
Buddalaz <sandunsampath289@gmail.com>
2022-10-20 15:07:06 +0530

    variables string in go

--- main.go
+++ main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello there")
+
+	//create/decalre/initialize a variables in go
+	//strings
+
+	//1.data type definition
+	var nameOne string = "one"
+	fmt.Println(nameOne)
+
+	//2.without data type
+	var nameTwo = "two"
+	fmt.Println(nameTwo)
+
+	//3.assign a variable later
+	var nameThree string
+	fmt.Println(nameThree)
+
+	nameThree = "three" //can't change data type like assign int value to varaible nameThree
+	fmt.Println(nameThree)
+
+	//4.without using var and use instead colun we only do this when initializing declaring the variable
+	//cant use this outside a function
+	nameFour := "four"
+	fmt.Println(nameFour)
+
 }

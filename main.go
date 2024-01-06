0df0c859f73bb758eb00236661187ae125c417ec
Buddalaz <sandunsampath289@gmail.com>
2024-01-06 23:00:01 +0530

    function

--- main.go
+++ main.go
@@ -1,57 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+// define a function in outside the main function
+func sayGreeting(value string) {
+	fmt.Printf("Good morning %v \n", value)
+}
+
+// single arguments
+func sayBye(value string) {
+	fmt.Printf("Goodbye %v \n", value)
+}
+
+// multiple arguments
+func cycleNames(s []string, f func(string)) { //function can be also pass as an arguments in this second argument accepted function is string
+	for _, v := range s {
+		f(v)
+	}
+}
+
+// return value from a function
+func circleArea(r float64) float64 { //in here we specify the return value after the parenthesis
+	return math.Pi * r * r
+}
 
 func main() {
 
-	//boolean
+	//calling a function
+	// sayGreeting("mario") //output -> Good morning mario
+	// sayGreeting("luigi") //output -> Good morning luigi
+	// sayBye("mario")      //output -> Goodbye mario
 
-	age := 45
-
-	fmt.Println(age <= 50) //age is grater than or equal 50, output-> true
-	fmt.Println(age >= 50) //age is less than or equal 50, output-> false
-	fmt.Println(age == 45) //age is equal 45, output-> true
-	fmt.Println(age != 50) //age is not 50, output-> true
-
-	//conditionals
-	if age < 30 {
-		fmt.Println("age is less than 30")
-	} else if age < 40 {
-		fmt.Println("age is less than 40")
-	} else {
-		fmt.Println("age is not less than 45")
-	}
-
-	names := []string{"aniaml", "birds", "fish", "reptails"}
-
-	for index, value := range names {
-		if index == 1 {
-			fmt.Println("continuing at pos", index)
-			continue
-		}
-		fmt.Printf("the value at pos %v is %v \n", index, value)
-	}
+	//calling multiple arguments
+	cycleNames([]string{"simon", "ksi", "vik123"}, sayGreeting) //we only pass the reference in the calling function
+	cycleNames([]string{"simon", "ksi", "vik123"}, sayBye)      //we only pass the reference in the calling function
 	//output is
-	// 	the value at pos 0 is aniaml
-	// continuing at pos 1
-	// the value at pos 2 is fish
-	// the value at pos 3 is reptails
+	// Good morning simon
+	// Good morning ksi
+	// Good morning vik123
+	// Goodbye simon
+	// Goodbye ksi
+	// Goodbye vik123
 
-	for index, value := range names {
-		if index == 1 {
-			fmt.Println("continuing at pos", index)
-			continue
-		}
-		if index > 2 {
-			fmt.Println("breaking at pos", index)
-			break
-		}
-		fmt.Printf("the value at pos %v is %v \n", index, value)
-	}
-	//output is
-	// 	the value at pos 0 is aniaml
-	// continuing at pos 1
-	// the value at pos 2 is fish
-	// breaking at pos 3
+	//return value from a function
+	a1 := circleArea(2.34)
+	a2 := circleArea(10)
 
+	fmt.Println("Area 1 is", a1) //output -> Area 1 is 17.20210473399627
+	fmt.Println("Area 2 is", a2) //output -> Area 2 is 314.1592653589793
+
+	fmt.Printf("circle 1 is %0.3f and circle 2 is %0.3f", a1, a2) //output -> circle 1 is 17.202 and circle 2 is 314.159
 }
